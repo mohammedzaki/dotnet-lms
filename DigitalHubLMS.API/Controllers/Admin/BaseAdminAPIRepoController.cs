1ace282c23f42a598e3dd28a884b78adca390368
Moahmed Zaki <mohamed.zaki@eye-ltd.com>
2021-10-22 20:22:41 +0200

    Add Genric Controller

---
+++ DigitalHubLMS.API/Controllers/Admin/BaseAdminAPIRepoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DigitalHubLMS.Core.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MZCore.Patterns.Repositroy;
+
+namespace DigitalHubLMS.API.Controllers.Admin
+{
+    [Route("admin/[controller]")]
+    public class BaseAdminAPIRepoController<TRepository, TEntity, TKey> : BaseAPIController
+        where TRepository : IRepository<TEntity, TKey>
+        where TEntity : class, IEntity<TKey>, new()
+        where TKey : struct,
+          IComparable,
+          IComparable<TKey>,
+          IConvertible,
+          IEquatable<TKey>,
+          IFormattable
+    {
+        protected readonly TRepository _repository;
+
+        public BaseAdminAPIRepoController(DigitalHubLMSContext context, TRepository repository)
+            : base(context)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public virtual async Task<ActionResult<List<TEntity>>> GetAll()
+        {
+            return await _repository.GetAll();
+        }
+
+        [HttpPost]
+        public virtual async Task<ActionResult<TEntity>> CreateOrUpdate(TEntity entity)
+        {
+            //return await _repository.CreateAsync(user);
+
+            return entity;
+        }
+
+        [HttpDelete("{id?}")]
+        public virtual async Task<ActionResult<int>> Delete(TKey? id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+    }
+}
--- DigitalHubLMS.API/Controllers/Admin/UsersController.cs
+++ DigitalHubLMS.API/Controllers/Admin/UsersController.cs
@@ -16,34 +16,11 @@ using DigitalHubLMS.API.Controllers.Admin;
 
 namespace DigitalHubLMS.API.Controllers.Admin
 {
-    public class UsersController : BaseAdminAPIController
+    public class UsersController : BaseAdminAPIRepoController<IUserRepository, User, long>
     {
-        private readonly IUserRepository _repository;
-
-        public UsersController(IUserRepository repository, DigitalHubLMSContext context)
-            : base(context)
+        public UsersController(DigitalHubLMSContext context, IUserRepository repository)
+            : base(context, repository)
         {
-            _repository = repository;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<List<User>>> GetAllUsers()
-        {
-            return await _repository.GetAll();
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<User>> CreateUser(User user)
-        {
-            //return await _repository.CreateAsync(user);
-
-            return user;
-        }
-
-        [HttpDelete("{id?}")]
-        public async Task<ActionResult<int>> DeleteUser(long? id)
-        {
-            return await _repository.DeleteAsync(id);
         }
     }
 }
