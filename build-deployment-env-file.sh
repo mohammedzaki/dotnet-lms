56e662f77ce4fb8187418aba77824e2693be6c03
Moahmed Zaki <mohamed.zaki@eye-ltd.com>
2021-11-15 06:53:52 +0200

    fixxxxxxxxxxxxx

--- build-deployment-env-file.sh
+++ build-deployment-env-file.sh
@@ -3,25 +3,24 @@
 #DB_SERVER_HOST="mssql-$CI_DEPLOYMENT_ENV_NAME"
 APP_SECRETS_NAME="dotnetcore-app-secrets"
 
-# cat > deployment-env.yml <<EOL
-# ---
-# apiVersion: v1
-# kind: Secret
-# metadata:
-#   name: $APP_SECRETS_NAME
-# type: Opaque
-# stringData:
-#   DB_CONNECTION: "mssql"
-#   DB_HOST: "$DB_SERVER_HOST"
-#   DB_PORT: "$DB_SERVER_PORT"
-#   DB_DATABASE: "$DB_SERVER_DATABASE"
-#   DB_USERNAME: "$DB_SERVER_USER"
-#   DB_PASSWORD: "$DB_SERVER_PASSWD"
-#   ASPNETCORE_ENVIRONMENT: "$CI_DEPLOYMENT_ENV_NAME"
-#   ConnectionStrings__DefaultConnection: "Server=$DB_SERVER_HOST;Database=$DB_SERVER_DATABASE;User Id=$DB_SERVER_USER;Password=$DB_SERVER_PASSWD;"
-#   ASPNETCORE_URLS: "https://+;http://+"
-#   LettuceEncrypt__PersistDataDirectory: "/app/gce-persistent-disk/LettuceEncrypt/"
-#   UseLettuceEncryptSSL: "$CI_UseLettuceEncryptSSL"
-#   JWT__ValidAudience: "lms.dh-labs.com"
-#   JWT__ValidIssuer: "test-hub.elschoola.com"
-# EOL
+cat > deployment-env.yml <<EOL
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: $APP_SECRETS_NAME
+type: Opaque
+stringData:
+  DB_CONNECTION: "mssql"
+  DB_HOST: "$DB_SERVER_HOST"
+  DB_PORT: "$DB_SERVER_PORT"
+  DB_DATABASE: "$DB_SERVER_DATABASE"
+  DB_USERNAME: "$DB_SERVER_USER"
+  DB_PASSWORD: "$DB_SERVER_PASSWD"
+  ASPNETCORE_ENVIRONMENT: "$CI_DEPLOYMENT_ENV_NAME"
+  ASPNETCORE_URLS: "https://+;http://+"
+  LettuceEncrypt__PersistDataDirectory: "/app/gce-persistent-disk/LettuceEncrypt/"
+  UseLettuceEncryptSSL: "$CI_UseLettuceEncryptSSL"
+  JWT__ValidAudience: "lms.dh-labs.com"
+  JWT__ValidIssuer: "test-hub.elschoola.com"
+EOL
